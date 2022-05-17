67969fa359ba0452ae23a2861e4002b14796bb26
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-17 10:18:04 -0400

    defined columns for product.js and completed index.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
 
 module.exports = {
   Product,
